7e9c3e0619ca35a9bae16e2849fc8032adf796fd
jsingle345 <jsingle345@gmail.com>
2020-11-01 11:12:50 -0600

    commit tree 69fb5b76f6bcbe4f240a0e10e8c92dee78de8036

--- index.js
+++ index.js
@@ -1,7 +1,72 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
+addingEventListener = () => {
+  const input = document.querySelector('#input');
 
-    input.addEventListener('click', function(event) {
-  alert('I was clicked!');
-    }); 
+  input.addEventListener('click', (e) => {
+    alert("You have been clicked");
+  }
+  )
 }
+
+addingEventListener();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addingEventListener() {
+//   const input = document.getElementById('input');
+
+//   input.addEventListener('click', function(event) {
+// alert('I was clicked!');
+//   }); 
+// }
